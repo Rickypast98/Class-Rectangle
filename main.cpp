2376a06ff88ba4eb5a27198c79ee54b88c4160cc
Rickypast98 <rickypast98@gmail.com>
2023-04-27 00:13:12 +0200

    modified code of main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,40 @@
 
 int main() {
 
-	Rectangle rectA(10,5);
-	Rectangle rectB = rectA;
-	Rectangle rectC, rectD;
-	
-	rectC = rectB;
-	
-	rectD.SetDim(1,1);
-	
-	if (rectC == rectB)
-		cout << "rectC has the same size of rectB " << endl;
-	else 
-		cout << "rectC has not the same size of rectB " << endl;
-	
-	
-	return 0;
+	int w;
+	int l;
 
+	int w1;
+	int l1;
+
+	int A;
+	int P;
+
+	Rectangle objRectA(10,5);
+	Rectangle objRectB;
+	Rectangle objRectC;
+	
+	objRectB.SetDim(3,7);
+	
+	objRectA.SetWidth(4);
+	
+	w = objRectB.GetLength();
+
+	l = objRectB.GetWidth();
+
+	objRectB.GetDim(w1 ,l1);
+
+	objRectC = objRectB;
+
+
+	if (objRectC == objRectB);
+	{
+		cout << "C and B are equal" << endl;
+	}
+
+	A = objRectA.GetArea();
+
+	P = objRectA.GetPerimeter();
+
+	return 0;
 }
